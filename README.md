417aa029a143dbacc76e43cae71ebc53acfb6d56
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-09-05 12:23:53 +1000

    questions

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 psudo code:
 
+1. user clicks the 'start quiz button'
+2. the intro page disappears and the first question appears + time coundown from 100. reduce 1 by every second
+3. the first quiz appears with 4 options
+4. if user picks the right question, go to the next quiz
+5. if user picks the wrong question, go to the next quiz and reduce score by 10
+6. when the score becames 0 OR user answers all 10 questions, show the score
+7. add the score into the highscore list
---
+++ script.js
@@ -0,0 +1,149 @@
+
+
+
+
+
+
+
+
+
+
+var questions = [
+
+    {
+        question: "What is the capital of Australia?",
+            answers: {
+                1: "Sydney",
+                2: "Melbourne",
+                3: "Perth",
+                4: "Canberra"
+            },
+            
+            correctAnswer: "4"
+
+    },
+
+    {
+        question: "What is the capital of the United States?",
+            answers: {
+                1: "Los Angeles",
+                2: "New York",
+                3: "Washington",
+                4: "Miami"
+            },
+            
+            correctAnswer: "3"
+
+    },
+
+    {
+        question: "What is the capital of Germany?",
+            answers: {
+                1: "Munich",
+                2: "Frankfurt",
+                3: "Berlin",
+                4: "Paris"
+            },
+            
+            correctAnswer: "3"
+
+    },
+
+    {
+        question: "What is the capital of New Zealand?",
+            answers: {
+                1: "Auckland",
+                2: "Christchurch",
+                3: "Queenstown",
+                4: "Wellington"
+            },
+            
+            correctAnswer: "4"
+
+    },
+
+
+    {
+        question: "What is the capital of Japan?",
+            answers: {
+                1: "Fukuoka",
+                2: "Tokyo",
+                3: "Osaka",
+                4: "Sapporo"
+            },
+            
+            correctAnswer: "2"
+
+    },
+
+
+    {
+        question: "What is the capital of the Philippines?",
+            answers: {
+                1: "Bangkok",
+                2: "Jakarta",
+                3: "Manila",
+                4: "Mumbai"
+            },
+            
+            correctAnswer: "3"
+
+    },
+
+    {
+        question: "What is the capital of the Egypt?",
+            answers: {
+                1: "Cairo",
+                2: "Capetown",
+                3: "Prague",
+                4: "Nairobi"
+            },
+            
+            correctAnswer: "1"
+
+    },
+
+    {
+        question: "Which of the following country is an island?",
+            answers: {
+                1: "Bangladesh",
+                2: "Panama",
+                3: "Israel",
+                4: "Kingdom of Tonga"
+            },
+            
+            correctAnswer: "4"
+
+    },
+
+
+    {
+        question: "Which of the following country has the largest population?",
+            answers: {
+                1: "Nigeria",
+                2: "Germany",
+                3: "Australia",
+                4: "Canada"
+            },
+            
+            correctAnswer: "1"
+
+    },
+
+
+
+    {
+        question: "Which of the following country does not have red in its flag?",
+            answers: {
+                1: "Barbados",
+                2: "Trinidad and Tobago",
+                3: "Spain",
+                4: "Columbia"
+            },
+            
+            correctAnswer: "1"
+
+    },
+
+
+]
